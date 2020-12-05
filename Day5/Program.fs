eb70059b252f85dc01cb157c1658b220f115a7c0
Tiago Abreu <tiagoalvesabreu@gmail.com>
2020-12-05 17:09:29 +0100

    Day 5 challenge

---
+++ Day5/Program.fs
@@ -0,0 +1,50 @@
+﻿module Program
+
+open Common.Files
+open System
+
+let rec calculateFromRange lowChar highChar (minRange:decimal) (maxRange:decimal) (values:char[]) =
+    if minRange = maxRange then
+        minRange
+    else
+        match values.[0] with
+            | x when x = lowChar -> calculateFromRange lowChar highChar minRange (floor (maxRange-(maxRange-minRange)/decimal(2))) values.[1 ..]
+            | y when y = highChar ->  calculateFromRange lowChar highChar (ceil (minRange+(maxRange-minRange)/decimal(2))) maxRange values.[1 ..]
+            | _ -> minRange
+
+let calculateSeat (seatCode:char[]) =
+    let row = calculateFromRange 'F' 'B' (0|>decimal) (127|>decimal) seatCode.[0..6]
+    let column = calculateFromRange 'L' 'R' (0|>decimal) (7|>decimal) seatCode.[7..9]
+    (row|>int,column|>int)
+
+let calculateId seat =
+    fst seat * 8 + snd seat
+
+let calculatePart1 seatCodes =
+    seatCodes
+    |> Array.map explode
+    |> Array.map calculateSeat
+    |> Array.map calculateId
+    |> Array.max
+
+let calculatePart1FromFile filePath =
+    filePath
+    |> getLineValuesFromFilePath
+    |> calculatePart1
+
+
+let calculatePart2 seatCodes =
+    seatCodes
+    |> Array.map explode
+    |> Array.map calculateSeat
+    |> Array.map calculateId
+    |> Array.sort
+    |> Array.mapi (fun i v -> (i, v))
+    |> Array.find (fun (i,v) -> i <> (v - 32))
+    |> snd
+
+let calculatePart2FromFile filePath =
+    filePath
+    |> getLineValuesFromFilePath
+    |> calculatePart2
+
---
+++ Day5/Tests.fs
@@ -0,0 +1,30 @@
+﻿module Tests
+
+open Xunit
+open Program
+open System
+open Common.Files
+
+[<Theory>]
+[<InlineData("FBFBBFFRLR", 357)>]
+[<InlineData("BFFFBBFRRR", 567)>]
+[<InlineData("FFFBBBFRRR", 119)>]
+[<InlineData("BBFFBBFRLL", 820)>]
+let ``Calculate Part1 Test Case`` (code, expected) =
+    let result = calculateId <| calculateSeat (explode code)
+    Assert.Equal (expected, result)
+
+[<Fact>]
+let ``Real Case Part1`` () =
+    let result = calculatePart1FromFile "input.txt"
+    Console.WriteLine result
+
+//[<Fact>]
+//let ``Calculate Part2 Test Case`` () =
+//    let result = calculatePart2FromFile "inputTest2.txt"
+//    Assert.Equal (4, result)
+
+[<Fact>]
+let ``Real Case Part2`` () =
+    let result = calculatePart2FromFile "input.txt"
+    Console.WriteLine (result - 1)
