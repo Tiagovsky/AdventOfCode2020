d12f914247c8cf7dc5449c718efa499bc4c0ed10
Tiago Abreu <tiagoalvesabreu@gmail.com>
2020-12-06 15:32:11 +0100

    Day 6 Challenge

---
+++ Day6/Program.fs
@@ -0,0 +1,44 @@
+﻿module Program
+
+open Common.Files
+open System
+
+
+let getTotalAnswersInGroup answers =
+    answers
+    |> explode
+    |> Array.distinct
+    |> Array.filter Char.IsLetter
+    |> Array.length
+
+let calculatePart1 answers =
+    answers
+    |> Array.map getTotalAnswersInGroup
+    |> Array.sum
+
+let calculatePart1FromFile filePath =
+    let fileContent = readFile filePath
+    
+    fileContent.Split ($"{Environment.NewLine}{Environment.NewLine}", StringSplitOptions.RemoveEmptyEntries)
+    |> calculatePart1
+
+let getCommonAnswersInGroup (answers:string) =
+    let numLines = answers.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Length
+
+    answers
+    |> explode
+    |> Array.filter Char.IsLetter
+    |> Array.countBy (fun c -> c)
+    |> Array.filter (fun (_,count) -> count = numLines)
+    |> Array.length
+
+let calculatePart2 answers =
+    answers
+    |> Array.map getCommonAnswersInGroup
+    |> Array.sum
+
+let calculatePart2FromFile filePath =
+    let fileContent = readFile filePath
+    
+    fileContent.Split ($"{Environment.NewLine}{Environment.NewLine}", StringSplitOptions.RemoveEmptyEntries)
+    |> calculatePart2
---
+++ Day6/UnitTest1.fs
@@ -0,0 +1,11 @@
+module Day6
+
+open NUnit.Framework
+
+[<SetUp>]
+let Setup () =
+    ()
+
+[<Test>]
+let Test1 () =
+    Assert.Pass()
