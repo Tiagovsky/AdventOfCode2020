7224385df366c9d558eb39b73c1b19a929e11bbe
Tiago Abreu <tiagoalvesabreu@gmail.com>
2020-12-08 18:07:38 +0100

    Day 7 challenge

---
+++ Day7/Program.fs
@@ -0,0 +1,84 @@
+﻿module Program
+
+open Common.Files
+open System
+
+let parseRule (rule:string) =
+    let mainBag = rule.[.. rule.IndexOf(" bags contain") - 1];
+    let contained = rule.[rule.IndexOf(" bags contain ") + 14 ..].Split(", ")
+                    |> Array.filter (fun s -> not (s.Contains("other bags")))
+                    |> Array.map (fun s -> (s.[2 .. s.IndexOf(" bag") - 1], s.[0]))
+
+    (mainBag, contained)
+
+let rec findBags desiredBag rules bagRule =
+    let containsBag = 
+        snd bagRule
+        |> Array.exists (fun (b, _) -> b = desiredBag)
+
+    let getRule name =
+        rules
+        |> Array.find (fun (r,_) -> r = name)
+
+    if (containsBag) then true
+    else
+        snd bagRule
+        |> Array.map (fun (c,_) -> getRule c)
+        |> Array.exists (findBags desiredBag rules)
+
+let calculatePart1 desiredBag rules =
+    let parsedRules = rules |> Array.map parseRule
+
+    parsedRules
+    |> Array.filter (findBags desiredBag parsedRules)
+    |> Array.length
+
+let calculatePart1FromFile (desiredBag:string) filePath =
+    filePath
+    |> getLineValuesFromFilePath
+    |> calculatePart1 desiredBag
+
+let getRuleInRules name rules =
+    rules
+    |> Array.find (fun (r,_) -> r = name)
+
+let rec countBags rule rules =
+    let inline charToInt c = int c - int '0'
+
+    let insideBags = 
+        snd rule
+        |> Array.map (fun (r,c) -> (r, c |> charToInt))
+
+    let insideBagsCount = 
+        insideBags
+        |> Array.map snd
+        |> Array.sum
+
+    let getCountInInsideBags name =
+        insideBags
+        |> Array.find (fun (r, c) -> r = name)
+        |> snd
+
+    if (insideBagsCount = 0) then
+        0
+    else
+        let insideRulesCount = 
+            snd rule
+            |> Array.map (fun (c,_) -> getRuleInRules c rules)
+            |> Array.map (fun r -> (countBags r rules) * (getCountInInsideBags (fst r)))
+            |> Array.sum
+
+        insideBagsCount + insideRulesCount
+
+let calculatePart2 desiredBag rules =
+    let parsedRules = rules |> Array.map parseRule
+    
+    let initialRule = getRuleInRules desiredBag parsedRules
+
+    parsedRules
+    |> countBags initialRule
+
+let calculatePart2FromFile (desiredBag:string) filePath =
+    filePath
+    |> getLineValuesFromFilePath
+    |> calculatePart2 desiredBag
---
+++ Day7/Tests.fs
@@ -0,0 +1,27 @@
+﻿module Tests
+
+open Xunit
+open Program
+open System
+
+[<Fact>]
+let ``Calculate Part1 Test Case`` () =
+    let result = calculatePart1FromFile "shiny gold" "inputTest.txt"
+    Assert.Equal (4, result)
+
+[<Fact>]
+let ``Real Case Part1`` () =
+    let result = calculatePart1FromFile "shiny gold" "input.txt"
+    Console.WriteLine result
+
+[<Theory>]
+[<InlineData("inputTest.txt", 32)>]
+[<InlineData("inputTest2.txt", 126)>]
+let ``Calculate Part2 Test Case`` (fileName, expected) =
+    let result = calculatePart2FromFile "shiny gold" fileName
+    Assert.Equal (expected, result)
+
+[<Fact>]
+let ``Real Case Part2`` () =
+    let result = calculatePart2FromFile "shiny gold" "input.txt"
+    Console.WriteLine (result)
